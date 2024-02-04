7b8cfda3efb91b6449e87fff57fc49ec6c763108
chaz <chazriver@gmail.com>
2024-02-03 23:49:32 -0800

    Pushing update for SharkSafeAI description

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -22,7 +22,9 @@ const Who: FC = () => {
         <p data-scroll className="text-lg text-gray-200" id="story">
         Greetings! My name is Chaz Hampton, I a software eningeer currently based in Las Vegas, Nevada. My journey into software development began at the age of 13, where I honed my skills by crafting tools and scripts. Over the years, I have excelled in numerous projects, collaborated in diverse team environments, and played a pivotal role in developing robust backend solutions during my tenure in the U.S. Air Force.
         <br></br>
+        <br></br>
         My passion lies in addressing real-world challenges through the creation of impactful and innovative products. While I possess comprehensive expertise in full-stack web development, my primary focus and dedication revolve around the intricate realm of mobile development.
+        <br></br>
         <br></br>
         Beyond the code, I find fulfillment in leisure activities such as gaming on my PS5, indulging in musical pursuits, navigating waters through kayaking, and fostering meaningful connections with friends. I am committed to leveraging my extensive skill set and experiences to contribute significantly to the ever-evolving landscape of mobile application development. 
         </p>
