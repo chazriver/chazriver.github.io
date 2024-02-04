19eb189a3de09262ae02dc66d33c6d1f23c32d41
chaz <chazriver@gmail.com>
2024-02-04 01:56:12 -0800

    Pushing update for SharkSafeAI description

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -65,7 +65,7 @@ const Contact: FC = () => {
                   src="/resume.png" width="100" height="100"
                   alt=""
                 />
-                <h1>{"Linkedin"}</h1>
+                <h1>{"My Resume"}</h1>
               </a>
 
               <a
