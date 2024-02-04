ac74438c2edced285f9c1a15acbca4e755728d49
chaz <chazriver@gmail.com>
2024-02-03 23:25:34 -0800

    Pushing update for SharkSafeAI

--- src/components/MainProjects.tsx
+++ src/components/MainProjects.tsx
@@ -12,7 +12,51 @@ const MainProjects: FC<MainProjectsProps> = ({ projects }) => {
     <>
       <h1 className="text-center text-4xl mb-10 md:mb-20" id = "myworksection">My Work</h1>
 
+      <div    //https://github.com/PKief/vscode-material-icon-theme  <-THEME ICONS
+          key={1}
+          className={`item flex gap-[20px] lg:gap-[50px] w-full px-[5vw] md:min-h-[60vh] mb-20 md:my-10 ${
+            1 % 2 === 1
+              ? "flex-col lg:flex-row"
+              : "flex-col lg:flex-row-reverse"
+          }`}
+        >
+          <div data-scroll data-scroll-speed="3" className="lg:flex-1">
+            <div className="border-[#888] border-2 rounded-[20px] overflow-hidden">
+              <div className="border-black border-[8px]">
+                <img
+                  className="w-full h-auto rounded-[12px]"
+                  src="/SharkSafeAI.png"//Project Image
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="item-info lg:flex-1">
+            <h1 className="text-3xl">{"SharkSafeAI - Embedded Aerial System"}</h1>
+            <p className="text-[20px] text-justify my-3">
+              {"Users have the flexibility to attach the ESP32-CAM to their privately owned drone or a comparable device, enabling manual flight over the designated observation area. This area is surveyed for silhouettes resembling that of a shark beneath the water."}
+            </p>
 
+            <div className="flex gap-[5px]">
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Farduino.svg&w=50&h=50" width="35" height="35" alt="Arduino"/>
+            <p> </p>
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fdrawio.svg&w=50&h=50" width="35" height="35" alt="draw.io"/>
+            <p>  </p>
+            </div>
+
+            <div className="flex mt-[25px] gap-[20px]">
+              <a
+                href={"./SharkSafeAI_Embedded_Aerial_System.pdf"}// Host paper on site f/ download link "./SharkSafeAI_Embedded_Aerial_System.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="h-[45px] px-[15px] text-white rounded transition duration-300 flex items-center gap-[10px] bg-[#1876d2] hover:bg-[#2884e0]"
+              >
+                <BiLinkExternal size={25} />
+                <span> Link </span>
+              </a>
+            </div>
+          </div>
+        </div>
 
 
       
@@ -42,11 +86,11 @@ const MainProjects: FC<MainProjectsProps> = ({ projects }) => {
             </p>
 
             <div className="flex gap-[5px]">
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fswift.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fswift.svg&w=50&h=50" width="35" height="35" alt="swift"/>
             <p> </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjava.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjava.svg&w=50&h=50" width="35" height="35" alt="java"/>
             <p>  </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fapplescript.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fapplescript.svg&w=50&h=50" width="35" height="35" alt="apple"/>
             <p> </p>
             <img src="/android.png" width="35" height="35" />
             </div>
@@ -94,13 +138,13 @@ const MainProjects: FC<MainProjectsProps> = ({ projects }) => {
             </p>
 
             <div className="flex gap-[5px]">
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fphp.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fphp.svg&w=50&h=50" width="35" height="35" alt="php"/>
             <p> </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fnginx.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fnginx.svg&w=50&h=50" width="35" height="35" alt="nginx"/>
             <p>  </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fhtml.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fhtml.svg&w=50&h=50" width="35" height="35" alt="html"/>
             <p> </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjavascript.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjavascript.svg&w=50&h=50" width="35" height="35" alt="js"/>
             </div>
 
             <div className="flex mt-[25px] gap-[20px]">
@@ -147,15 +191,15 @@ const MainProjects: FC<MainProjectsProps> = ({ projects }) => {
             </p>
 
             <div className="flex gap-[5px]">
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Ffirebase.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Ffirebase.svg&w=50&h=50" width="35" height="35" alt="FB"/>
             <p> </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fswift.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fswift.svg&w=50&h=50" width="35" height="35" alt="swift"/>
             <p> </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjava.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjava.svg&w=50&h=50" width="35" height="35" alt="java"/>
             <p>  </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fapplescript.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fapplescript.svg&w=50&h=50" width="35" height="35" alt="apple"/>
             <p> </p>
-            <img src="/android.png" width="35" height="35" />
+            <img src="/android.png" width="35" height="35" alt="android"/>
             </div>
 
             <div className="flex mt-[25px] gap-[20px]">
@@ -192,15 +236,15 @@ const MainProjects: FC<MainProjectsProps> = ({ projects }) => {
             </p>
 
             <div className="flex gap-[5px]">
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Ffirebase.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Ffirebase.svg&w=50&h=50" width="35" height="35" alt="FB"/>
             <p> </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fhtml.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fhtml.svg&w=50&h=50" width="35" height="35" alt="html"/>
             <p> </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjavascript.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjavascript.svg&w=50&h=50" width="35" height="35" alt="js"/>
             <p>  </p>
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fcss.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fcss.svg&w=50&h=50" width="35" height="35" alt="css"/>
             <p> </p>
-            <img src="/android.png" width="35" height="35" />
+            <img src="/android.png" width="35" height="35" alt="android"/>
             </div>
 
             <div className="flex mt-[25px] gap-[20px]">
@@ -236,9 +280,9 @@ const MainProjects: FC<MainProjectsProps> = ({ projects }) => {
             </p>
 
             <div className="flex gap-[5px]">
-            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjava.svg&w=50&h=50" width="35" height="35" />
+            <img src="https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2Fjava.svg&w=50&h=50" width="35" height="35" alt="java"/>
             <p> </p>
-            <img src="/android.png" width="35" height="35" />
+            <img src="/android.png" width="35" height="35" alt="android"/>
             </div>
 
             <div className="flex mt-[25px] gap-[20px]">
