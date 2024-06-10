31dc262f5c4c5dbea503e900281195092502e071
Chaz Hampton <chazhampton@Chazs-MacBook-Pro.local>
2024-06-10 10:43:51 -0700

    updating for bloomberg

--- src/components/SmallProjects.tsx
+++ src/components/SmallProjects.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react";
-import { allDataType } from "../shared/types";
+import { FC } from 'react';
+import { allDataType } from '../shared/types';
 
 interface SmallProjectsProps {
-  projects: allDataType["smallProjects"];
+  projects: allDataType['smallProjects'];
 }
 
 const SmallProjects: FC<SmallProjectsProps> = ({ projects }) => {
@@ -12,74 +12,89 @@ const SmallProjects: FC<SmallProjectsProps> = ({ projects }) => {
         <h1 className="text-4xl text-center">Places I have worked</h1>
         <p className="mt-4 mb-10 text-gray-400 text-lg text-center">{``}</p>
 
-
         <div
           data-scroll
           data-scroll-speed="1"
           className="grid gap-2 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] md:grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))]"
         >
+          <a
+            href={''}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex gap-3 hover:bg-slate-900 p-3 transition duration-300"
+            key={1}
+          >
+            <img
+              className="h-[40px] w-[40px] my-1 object-cover"
+              src={'/usaf.png'}
+              alt=""
+            />
+            <div>
+              <h1>U.S. Air Force</h1>
+              <p className="text-gray-400">
+                {'Cyber Operations Officer, Software Engineer'}
+              </p>
+            </div>
+          </a>
 
-            <a
-              href={""}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex gap-3 hover:bg-slate-900 p-3 transition duration-300"
-              key={1}
-            >
-              <img
-                className="h-[40px] w-[40px] my-1 object-cover"
-                src={"/usaf.png"}
-                alt=""
-              />
-              <div>
-                <h1>U.S. Air Force</h1>
-                <p className="text-gray-400">{"Cyber Operations Officer, Software Engineer"}</p>
-              </div>
-            </a>
+          <a
+            href={''}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex gap-3 hover:bg-slate-900 p-3 transition duration-300"
+            key={1}
+          >
+            <img
+              className="h-[40px] w-[40px] my-1 object-cover"
+              src={'/bubblein_logo.png'}
+              alt=""
+            />
+            <div>
+              <h1>Bubble In</h1>
+              <p className="text-gray-400">{'Software Developer'}</p>
+            </div>
+          </a>
 
-            <a
-              href={""}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex gap-3 hover:bg-slate-900 p-3 transition duration-300"
-              key={1}
-            >
-              <img
-                className="h-[40px] w-[40px] my-1 object-cover"
-                src={"/bubblein_logo.png"}
-                alt=""
-              />
-              <div>
-                <h1>Bubble In</h1>
-                <p className="text-gray-400">{"Software Developer"}</p>
-              </div>
-            </a>
+          <a
+            href={''}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex gap-3 hover:bg-slate-900 p-3 transition duration-300"
+            key={1}
+          >
+            <img
+              className="h-[40px] w-[40px] my-1 object-cover"
+              src={'/csusb.png'}
+              alt=""
+            />
+            <div>
+              <h1>California State University, San Bernardino</h1>
+              <p className="text-gray-400">
+                {'Instructional Associate, Software Engineering'}
+              </p>
+            </div>
+          </a>
 
-            <a
-              href={""}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex gap-3 hover:bg-slate-900 p-3 transition duration-300"
-              key={1}
-            >
-              <img
-                className="h-[40px] w-[40px] my-1 object-cover"
-                src={"/csusb.png"}
-                alt=""
-              />
-              <div>
-                <h1>California State University, San Bernardino</h1>
-                <p className="text-gray-400">{"Instructional Associate, Software Engineering"}</p>
-              </div>
-            </a>
-
-
+          <a
+            href={''}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex gap-3 hover:bg-slate-900 p-3 transition duration-300"
+            key={1}
+          >
+            <img
+              className="h-[40px] w-[40px] my-1 object-cover"
+              src={'/bloomberg.png'}
+              alt=""
+            />
+            <div>
+              <h1>Bloomberg L.P.</h1>
+              <p className="text-gray-400">{'Software Engineer Intern'}</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
-
-
-
   );
 };
 
