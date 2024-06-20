bc6970a25e3f87d808ed53155941515430adf07a
Chaz Hampton <chazhampton@Chazs-MacBook-Pro.local>
2024-06-20 11:25:11 -0700

    Spelling correction

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC } from 'react';
 
 const Who: FC = () => {
   return (
@@ -20,13 +20,25 @@ const Who: FC = () => {
 
         {/* Hide my age :v */}
         <p data-scroll className="text-lg text-gray-200" id="story">
-        My name is Chaz Hampton, I am a software eningeer currently residing in Las Vegas, Nevada. My journey into software development began at the age of 13, where I honed my skills by crafting tools and scripts. Over the years, I have excelled in numerous projects, collaborated in diverse team environments, and played a pivotal role in developing robust backend solutions during my tenure in the U.S. Air Force.
-        <br></br>
-        <br></br>
-        My passion lies in addressing real-world challenges through the creation of impactful and innovative products. I possess comprehensive expertise in full-stack web development, but have also found a calling in mobile development.
-        <br></br>
-        <br></br>
-        Beyond the code, I find fulfillment in leisurely activities such as gaming on my PS5, listening to music, kayaking, and just spending time with friends. I am committed to leveraging my extensive skill set and experiences to contribute to the ever-evolving landscape of software engineering. 
+          My name is Chaz Hampton, I am a software engineer currently residing
+          in Las Vegas, Nevada. My journey into software development began at
+          the age of 13, where I started crafting tools and scripts. Over the
+          years, I have excelled in numerous projects, collaborated within
+          diverse team environments, and played a pivotal role in developing
+          robust backend solutions during my tenure in the U.S. Air Force.
+          <br></br>
+          <br></br>
+          My passion lies in addressing real-world challenges through the
+          creation of impactful and innovative products. I possess comprehensive
+          expertise in full-stack web development, but have also found a calling
+          in mobile development.
+          <br></br>
+          <br></br>
+          Beyond the code, I find fulfillment in leisurely activities such as
+          gaming on my PS5, listening to music, kayaking, and just spending time
+          with friends. I am committed to leveraging my extensive skill set and
+          experiences to contribute to the ever-evolving landscape of software
+          engineering.
         </p>
       </div>
     </div>
